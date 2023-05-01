b878391072c87f1dc2fd89c719a461102504be1c
Extro21 <vostrik21@gmail.com>
2023-05-01 21:19:01 +0300

    sprint 9 subject 2

--- app/src/main/java/com/practicum/playlistmarket/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmarket/SearchActivity.kt
@@ -1,11 +1,90 @@
 package com.practicum.playlistmarket
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var edText : EditText
+    //var cnt : String =""
+
+    companion object {
+        const val SEARCH_QUERY = "SEARCH_QUERY"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_QUERY, edText.text.toString())
+    }
+
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        val searchValue = savedInstanceState.getString(SEARCH_QUERY,"")
+//        edText.setText(searchValue)
+//    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+
+
+        val btClear = findViewById<ImageView>(R.id.btClear)
+        edText = findViewById(R.id.editSearch)
+
+
+        btClear.setOnClickListener{
+            edText.setText("")
+            val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            keyboard.hideSoftInputFromWindow(edText.windowToken, 0) // скрыть клавиатуру
+            edText.clearFocus()
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                btClear.visibility = clearButtonVisibility(s)
+
+                if (savedInstanceState != null) {
+                    val searchValue = savedInstanceState.getString(SEARCH_QUERY,"")
+                    edText.setText(searchValue)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //empty
+            }
+
+        }
+
+        edText.addTextChangedListener(simpleTextWatcher)
+
+
     }
+
+    fun clearButtonVisibility(s : CharSequence?) : Int{
+        return if(s.isNullOrEmpty()){
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+
+    }
+
+    fun clickToolBar(view: View) {
+        finish()
+    }
+
 }
--- app/src/main/java/com/practicum/playlistmarket/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmarket/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package com.practicum.playlistmarket
+
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
@@ -12,47 +16,68 @@ import androidx.appcompat.app.AppCompatDelegate
 class SettingsActivity : AppCompatActivity() {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val shareApp = findViewById<TextView>(R.id.share_app)
+        val shareApp = findViewById<LinearLayout>(R.id.share_app)
+        val support = findViewById<LinearLayout>(R.id.support)
+        val termsOfUse = findViewById<LinearLayout>(R.id.termsОfUse)
+
         shareApp.setOnClickListener {
-          // val intent = Intent(Intent.ACTION_SEND)
-            val intent = Intent().apply {
-                action = Intent.ACTION_SEND
-            }
-            intent.type = "plain/text";
-            intent.putExtra(Intent.EXTRA_EMAIL, "vlad@yandex.ru")
-            intent.putExtra(Intent.EXTRA_SUBJECT, "go")
-            intent.putExtra(Intent.EXTRA_TEXT, "hello")
-            startActivity(Intent.createChooser(intent,
-                "Отправка письма..."));
+            val massage = "https://practicum.yandex.ru/android-developer/"
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, massage)
+            startActivity(Intent.createChooser(intent, "Отправляем текст"))
         }
 
 
-//        toolbar.setNavigationOnClickListener { onBackPressed() }
+        support.setOnClickListener {
+            val address = "vostrik16@uandex.ru"
+
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "Спасибо разработчикам и разработчицам за крутое приложение!"
+                )
+            }
+            startActivity(intent)
 
 
+        }
+
+
+        termsOfUse.setOnClickListener {
+            val address = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, address)
+            startActivity(intent)
+        }
 
     }
 
 
-
-    fun nightTheme(view: View){
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    fun nightTheme(view: View) {
         val switch = findViewById<Switch>(R.id.switch_setting)
-        if(switch.isChecked){
+        if (switch.isChecked) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
-    fun clickToolBar(view: View){
+    fun clickToolBar(view: View) {
         finish()
     }
-
-
 
 
 }
