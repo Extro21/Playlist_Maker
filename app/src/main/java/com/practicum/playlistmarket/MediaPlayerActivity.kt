124713e51076f166e8089a71caa7ed6d5ba206a9
Extro21 <vostrik21@gmail.com>
2023-06-23 14:20:30 +0300

    sprint 13 subject

--- app/src/main/java/com/practicum/playlistmarket/MediaPlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmarket/MediaPlayerActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmarket.databinding.ActivityMediaPlayerBinding
-
 import java.text.SimpleDateFormat
 import java.util.*
 
